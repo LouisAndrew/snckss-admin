82e8a9d71466166009a0bf6e9f7be7fc23d223a7
Louis Andrew <louisandrew3@gmail.com>
2020-07-07 20:49:05 +0200

    adding list to categories

--- src/components/creator-pages/categories/index.tsx
+++ src/components/creator-pages/categories/index.tsx
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import { Icon } from '@iconify/react'
 import arrowBack from '@iconify/icons-mdi/arrow-back'
+import { useFirestore } from 'reactfire'
 
 import './styles.scss'
 import Editor from './editor'
+import List, { ListItem } from '../../list'
+import { Category } from '../../../interfaces/category'
+
+const initialCategories: Category[] = []
 
 const Categories: React.FC = () => {
         const [isEditing, setIsEditing] = useState(true)
+        const [ctg, setCtg] = useState(initialCategories)
+
+        const categories = useFirestore().collection('categories')
+
+        useEffect(() => {
+                categories.get().then((docs) => {
+                        docs.forEach((doc) => {
+                                const category = doc.data() as Category
+                                setCtg([...ctg, category])
+                        })
+                })
+        })
+
+        const handleRemove = () => {}
 
         const goBack = () => {
                 if (isEditing) {
@@ -14,13 +33,30 @@ const Categories: React.FC = () => {
                 }
         }
 
+        const items: ListItem[] = ctg.map((category) => {
+                const { name } = category
+                const item: ListItem = {
+                        text: name,
+                        key: name,
+                }
+                return item
+        })
+
         return (
                 <div className="categories">
                         <div className="back" onClick={goBack}>
                                 <Icon icon={arrowBack} />
                         </div>
                         <h1>Categories</h1>
-                        {isEditing && <Editor goBack={goBack} />}
+                        {isEditing ? (
+                                <Editor goBack={goBack} />
+                        ) : (
+                                <List
+                                        headerText=""
+                                        items={items}
+                                        handleRemove={handleRemove}
+                                />
+                        )}
                 </div>
         )
 }
