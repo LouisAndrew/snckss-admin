b3943eab5bed3aad7d4daee2dc493370b495d46b
Louis Andrew <louisandrew3@gmail.com>
2020-07-11 23:33:58 +0200

    fixed export errors

---
+++ src/interfaces/index.ts
@@ -0,0 +1,12 @@
+// import { Category } from './category'
+// import { Brand } from './brand'
+// import { Order } from './order'
+// import { Product } from './product'
+// import { Admin } from './admin'
+
+// export { Category, Brand, Order, Product, Admin }
+export * from './category'
+export * from './brand'
+export * from './order'
+export * from './product'
+export * from './admin'
--- src/interfaces/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Category } from './category'
-import { Brand } from './brand'
-import { Order } from './order'
-import { Product } from './product'
-import { Admin } from './admin'
-
-export type { Category, Brand, Order, Product, Admin }
